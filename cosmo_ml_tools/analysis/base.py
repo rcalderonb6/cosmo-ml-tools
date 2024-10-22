c019306e2e38e2e0c895a6107cd006dc253188c8
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-22 20:45:48 +0200

    minor changes

--- cosmo_ml_tools/analysis/base.py
+++ cosmo_ml_tools/analysis/base.py
@@ -50,3 +50,68 @@ class ChainBase(ABC):
         """
         ...
         
+class AnalysisBase(ABC):
+    """
+    Abstract Analysis Class
+    """
+    def __init__(self) -> None:
+        pass
+    
+    def set_labels(self,labels) -> None:
+        self._labels=labels
+        
+    @abstractmethod
+    def computeEvidence(self):
+        ...
+        
+    @abstractmethod
+    def load(self,chains):
+        ...    
+    
+    @abstractmethod
+    def add_chain(chain:str,label:str,root:str):
+        ...
+        
+    @abstractmethod
+    def plot_triangle(self,params:list[str]=None):
+        ...    
+    
+    @abstractmethod
+    def plot_2D(self):
+        ...    
+    
+    @abstractmethod
+    def plot_posterior_y(self,x,f,theta):
+        ...    
+    
+    @abstractmethod
+    def _getGelmanRubin(self):
+        ...   
+    
+    @abstractmethod
+    def getInfo(self):
+        ...    
+    
+    @abstractmethod
+    def set_aliases(self,aliases):
+        ...        
+        
+    @property
+    def GelmanRubin(self) -> list:
+        self._getGelmanRubin(self)
+        
+    @property
+    def chains(self) -> dict:
+        return self._chains
+    
+    @property
+    def labels(self) -> list:
+        return self._labels
+    
+    @property
+    def filenames(self)-> list:
+        return self._filenames
+    
+    @property
+    def N(self) -> int:
+        return len(self._labels)
