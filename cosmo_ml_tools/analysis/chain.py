b366e81a600230a7eca58d1629854128903c5015
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-22 22:49:57 +0200

    test

--- cosmo_ml_tools/analysis/chain.py
+++ cosmo_ml_tools/analysis/chain.py
@@ -1,6 +1,25 @@
-class Chain:
+from base import ChainBase
+class MHChain(ChainBase):    
     """
-    A Base class for Chain
+    Metropolis-Hastings Base Class
     """
-    def __init__(self):
-        pass
+    def load(self):
+        return NotImplementedError
+    
+    def to_harmonic(self):
+        return NotImplementedError
+    
+    def getInfo(self):
+        return NotImplementedError    
+    
+    def getTable(self, params: list):
+        return NotImplementedError
+    
+    def set_param_labels(self,labels:list[str]):
+        return NotImplementedError
+
+class NSChain(ChainBase):
+    """
+    Nested-Sampling Base Class.
+    """
+    pass    
