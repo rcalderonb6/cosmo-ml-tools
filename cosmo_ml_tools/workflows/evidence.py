ed22e847d8357d51bfa3a6a058cc4b4fec765124
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-22 15:54:29 +0200

    test

--- cosmo_ml_tools/workflows/evidence.py
+++ cosmo_ml_tools/workflows/evidence.py
@@ -4,7 +4,7 @@ the Harmonic Mean estimator and Normalizing Flows.
 We use the python package Harmonic and this basic 
 example workflow heavily relies on their tutorial.
 """
-import harmonic as hm
+# import harmonic as hm
     
 def main(samples : object, ndim :int , 
          model: str = 'NVP', N_chains=4,epochs_num=20,
@@ -16,35 +16,35 @@ def main(samples : object, ndim :int ,
     samples, lnprob = samples.to_harmonic()
     
     # Instantiate harmonic's chains class
-    chains = hm.Chains(ndim)
-    chains.add_chains_3d(samples, lnprob)
+    # chains = hm.Chains(ndim)
+    # chains.add_chains_3d(samples, lnprob)
     
     # Split the chains into the ones which will be used to train the machine
     # learning model and for inference
-    chains_train, chains_infer = hm.utils.split_data(chains, training_proportion=training_proportion)
+    # chains_train, chains_infer = hm.utils.split_data(chains, training_proportion=training_proportion)
     # n_scaled_layers = 2
     # n_unscaled_layers = 4
     
-    if model=='NVP':
-        model = hm.model.RealNVPModel(ndim,standardize=True,temperature=temperature,**hm_kwargs)
+    # if model=='NVP':
+        # model = hm.model.RealNVPModel(ndim,standardize=True,temperature=temperature,**hm_kwargs)
     
     # Train model
-    model.fit(chains_train.samples, epochs=epochs_num, verbose= True)
+    # model.fit(chains_train.samples, epochs=epochs_num, verbose= True)
     
     samples = samples.reshape((-1, ndim))
     samp_num = samples.shape[0]
-    flow_samples = model.sample(samp_num)
-    hm.utils.plot_getdist_compare(samples, flow_samples)
+    # flow_samples = model.sample(samp_num)
+    # hm.utils.plot_getdist_compare(samples, flow_samples)
     
     # Instantiate harmonic's evidence class
-    ev = hm.Evidence(chains_infer.nchains, model)
+    # ev = hm.Evidence(chains_infer.nchains, model)
 
     # Pass the evidence class the inference chains and compute the evidence!
-    ev.add_chains(chains_infer)
-    ln_inv_evidence = ev.ln_evidence_inv
-    err_ln_inv_evidence = ev.compute_ln_inv_evidence_errors()
+    # ev.add_chains(chains_infer)
+    # ln_inv_evidence = ev.ln_evidence_inv
+    # err_ln_inv_evidence = ev.compute_ln_inv_evidence_errors()
     
-    print(f'ln inverse evidence (harmonic) = {-ln_inv_evidence} +/- {err_ln_inv_evidence}')
+    # print(f'ln inverse evidence (harmonic) = {-ln_inv_evidence} +/- {err_ln_inv_evidence}')
     return
 
 if __name__=='__main__':
