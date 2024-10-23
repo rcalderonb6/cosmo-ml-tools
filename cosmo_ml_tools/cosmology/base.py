139b1fb644f390891fb87ddcdf479eeb971eb49c
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-23 22:45:16 +0200

    add rough structure to cosmology module

---
+++ cosmo_ml_tools/cosmology/base.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+class BoltzmannBase(ABC):
+    """
+    Abstract Base Class for the Boltzmann Solver class
+    """
+    def __init__(self,*args,**kwargs) -> None:
+        pass
---
+++ cosmo_ml_tools/cosmology/classy.py
@@ -0,0 +1,6 @@
+from .base import BoltzmannBase
+
+class Class(BoltzmannBase):
+    """Base Class for the Boltzmann solver Class and its variations"""
+    def __init__(self) -> None:
+        pass
