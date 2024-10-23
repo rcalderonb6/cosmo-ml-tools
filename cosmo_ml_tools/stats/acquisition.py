2c0a5f4519ff4e16d42ed6fe19ace258e59078c3
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-23 16:05:02 +0200

    test acquisition and numpyro dependencies

---
+++ cosmo_ml_tools/stats/acquisition.py
@@ -0,0 +1,62 @@
+from typing import Dict, Type
+import jax.numpy as jnp
+import jax.random as jra
+import numpyro.distributions as dist
+
+from .gp import GaussianProcessJax
+
+def EI(rng_key: jnp.ndarray, model: Type[GaussianProcessJax],
+       X: jnp.ndarray, xi: float = 0.01,
+       maximize: bool = False, n: int = 1) -> jnp.ndarray:
+    """
+    Expected Improvement
+    """
+    y_mean, y_sampled = model.predict(rng_key, X, n=n)
+    if n > 1:
+        y_sampled = y_sampled.reshape(n * y_sampled.shape[0], -1)
+    mean, sigma = y_sampled.mean(0), y_sampled.std(0)
+    u = (mean - y_mean.max() - xi) / sigma
+    u = -u if not maximize else u
+    normal = dist.Normal(jnp.zeros_like(u), jnp.ones_like(u))
+    ucdf = normal.cdf(u)
+    updf = jnp.exp(normal.log_prob(u))
+    return sigma * (updf + u * ucdf) 
+
+
+def UCB(rng_key: jnp.ndarray, model: Type[GaussianProcessJax],
+        X: jnp.ndarray, beta: float = .25,
+        maximize: bool = False, n: int = 1) -> jnp.ndarray:
+    """
+    Upper confidence bound
+    """
+    _, y_sampled = model.predict(rng_key, X, n=n)
+    if n > 1:
+        y_sampled = y_sampled.reshape(n * y_sampled.shape[0], -1)
+    mean, var= y_sampled.mean(0), y_sampled.var(0)
+    delta = jnp.sqrt(beta * var)
+    if maximize:
+        return mean + delta
+    return mean - delta 
+
+
+def UE(rng_key: jnp.ndarray,
+       model: Type[GaussianProcessJax],
+       X: jnp.ndarray, n: int = 1) -> jnp.ndarray:
+    """Uncertainty-based exploration (aka kriging)"""
+    _, y_sampled = model.predict(rng_key, X, n=n)
+    return y_sampled.var(0)
+
+
+def Thompson(rng_key: jnp.ndarray,
+             model: Type[GaussianProcessJax],
+             posterior_samples: Dict[str, jnp.ndarray],
+             X: jnp.ndarray, n: int = 1) -> jnp.ndarray:
+    """Thompson sampling"""
+    posterior_samples = model.get_samples()
+    idx = jra.randint(rng_key, (1,), 0, len(posterior_samples["k_length"]))
+    samples = {k: v[idx] for (k, v) in posterior_samples.items()}
+    _, tsample = model.predict(rng_key, X, samples, n)
+    if n > 1:
+        tsample = tsample.mean(1)
+    return tsample.squeeze()
+     
--- docs/index.md
+++ docs/index.md
@@ -5,7 +5,7 @@
 
 [![image](https://img.shields.io/pypi/v/cosmo-ml-tools.svg)](https://pypi.python.org/pypi/cosmo-ml-tools)
 
-**My humble attempt to wrap many useful python packagages, ML algorithms and automate common workflows in Cosmology.**
+**An attempt to wrap many useful python packagages, ML algorithms and automate common workflows in Cosmology.**
 
 **DISCLAIMER**: This is very much work in progress. It is originally intended for my own use, so the documentation might not be as good as I would like it to be.
 
