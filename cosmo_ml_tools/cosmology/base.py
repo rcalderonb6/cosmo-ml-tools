61e305983f08863320b5b04c66a0951304af5448
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-23 23:30:48 +0200

    docstrings

--- cosmo_ml_tools/cosmology/base.py
+++ cosmo_ml_tools/cosmology/base.py
@@ -6,3 +6,40 @@ class BoltzmannBase(ABC):
     """
     def __init__(self,*args,**kwargs) -> None:
         pass
+    
+    @abstractmethod
+    def getInfo(self) -> None:
+        """
+        Print general information about the current Cosmology
+        """
+        ...
+        
+    @abstractmethod
+    def update(self,**kwargs) -> None:
+        """
+        Update the values of the cosmological parameters with the provided dictionary and recompute observables.
+        """
+        ...
+    
+    @abstractmethod
+    def compute(self) -> None:
+        """
+        (Re)-compute the observables for the chosen cosmology
+        """
+        ...
+        
+    @abstractmethod
+    def plot(self, observables: list[str]) -> None:
+        """
+        Plot one or more of the requested output observables
+
+        Args:
+            observables (list[str]): One of the output observables requested to Class. e.g. P(k), Cl's...
+        """
+        ...
+        
+    @abstractmethod
+    def store(self,filename) -> None:
+        """Store the requested outputs in a given folder
+        """
+        ...
