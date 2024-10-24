f890231dce7b888742c515e1e4e56c4a38a1f844
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-24 21:23:53 +0200

    typo fix

--- cosmo_ml_tools/plots/plot.py
+++ cosmo_ml_tools/plots/plot.py
@@ -19,41 +19,31 @@ def plot_fill_between(x : np.ndarray, samples : np.ndarray, label:str = None,
     ax.plot(x, median, label=label, c=color, ls='-', lw=lw)
     
     
-def plot_samples_y_lkl(x:np.ndarray,samples_y:np.ndarray,samples_z:np.ndarray,
-                       nsamples:int=75,seed:int|None = None,
-                       quiet:bool=True,cbar_lbl:str=r'$-\ln{\mathcal{L}}$',alpha:float=0.5,lw:float=0.5):
+# def plot_colorcoded_y(x:np.ndarray,samples_y:np.ndarray,samples_z:np.ndarray,
+#                        nsamples:int=75,seed:int|None = None,
+#                        quiet:bool=True,cbar_lbl:str=r'$-\ln{\mathcal{L}}$',alpha:float=0.5,lw:float=0.5):
 
-  from matplotlib.collections import LineCollection
-  if seed is not None:
-    random_state = np.random.default_rng(10)
-    ind=random_state.integers(low=0,high=len(samples_y), size=nsamples)
-  else:
-    ind=np.random.randint(len(samples_y),size=nsamples)
+#   from matplotlib.collections import LineCollection
+#   if seed is not None:
+#     random_state = np.random.default_rng(10)
+#     ind=random_state.integers(low=0,high=len(samples_y), size=nsamples)
+#   else:
+#     ind=np.random.randint(len(samples_y),size=nsamples)
     
-  ys=samples_y[ind]
+#   ys=samples_y[ind]
 
-  # Make a sequence of (x, y) pairs
-  line_segments= LineCollection([np.column_stack([x, y]) for y in ys],linestyles='solid',lw=lw,alpha=alpha)
-  line_segments.set_array(samples_z[ind])
+#   # Make a sequence of (x, y) pairs
+#   line_segments= LineCollection([np.column_stack([x, y]) for y in ys],linestyles='solid',lw=lw,alpha=alpha)
+#   line_segments.set_array(samples_z[ind])
 
-  ax.add_collection(line_segments)
-  fig.subplots_adjust(left=.09,right=.89,top=.9,wspace=.09,hspace=.08)
-  cbar_ax = fig.add_axes([0.92,0.11,0.02,0.79])
-  axcb = fig.colorbar(line_segments,cax=cbar_ax)
-  axcb.set_label(cbar_lbl,fontsize='xx-large')
+#   ax.add_collection(line_segments)
+#   fig.subplots_adjust(left=.09,right=.89,top=.9,wspace=.09,hspace=.08)
+#   cbar_ax = fig.add_axes([0.92,0.11,0.02,0.79])
+#   axcb = fig.colorbar(line_segments,cax=cbar_ax)
+#   axcb.set_label(cbar_lbl,fontsize='xx-large')
 
-  ax.legend()
-  # plt.sci(line_segments)# This allows interactive changing of the colormap.
-  if not quiet:
-      return fig,ax
+#   ax.legend()
+#   # plt.sci(line_segments)# This allows interactive changing of the colormap.
+#   if not quiet:
+#       return fig,ax
     
-  ''' 
-  Plot n random samples from the MCMC chains. 
-  Must input:
-  - z is the array of values where the samples of fde are computed
-  - samples_fde : a (flattened) array with samples of fde from the chains and corresponding likelihoods.
-  - samples_z : an array with the corresponding likelihood values
-  - nsamples:  The number of samples to plot from the chains (default: randomly selected
-  - figax = (fig,ax) is a tupple to specify the figure and axes 
-  - seed : specifies the seed for reproducibility (i.e. drawing the same samples)
-  '''
