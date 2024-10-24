bf3a317bc3ded0d3431188e9179cf10167714121
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-24 19:56:20 +0200

    docstrings formatting

--- cosmo_ml_tools/stats/crossing.py
+++ cosmo_ml_tools/stats/crossing.py
@@ -17,7 +17,7 @@ def get_Chebyshev_T(x:float|np.ndarray,Ci:list|np.ndarray):
     return cheb
 
 def analytical_fde_from_w(z,C0=1,C1=0,C2=0,C3=0,C4=0,C5=0,C6=0):
-    """Compute the integral fde=exp[\int 3(1+w) dln(1+z)] for a Chebyshev expansion of w(z) up to order 7 (C6)
+    """Compute the integral $f_\mathrm{DE}=\exp[\int 3(1+w) d\ln(1+z)]$ for a Chebyshev expansion of w(z) up to order 7 (C6)
 
     Args:
         z (array): redshift array
@@ -30,13 +30,13 @@ def analytical_fde_from_w(z,C0=1,C1=0,C2=0,C3=0,C4=0,C5=0,C6=0):
         C6 (int, optional): Seventh Chebyshev coefficient. Defaults to 0.
 
     Returns:
-        array: corresponding dark energy density evolution fde(z).
+        array: corresponding dark energy density evolution $f_\mathrm{DE}(z)$.
     """
     zmax=z.max()
     return (1 + z)**((3*zmax*(-128*C4*zmax + zmax**2*(-256*C4 - 8*(C2 + 20*C4)*zmax + 2*(C1 - 4*(C2 + 4*C4))*zmax**2 - (-1 + C0 - C1 + C2 + C4)*zmax**3 + C3*(2 + zmax)*(16 + zmax*(16 + zmax))) + C5*(2 + zmax)*(256 + zmax*(4 + zmax)*(128 + zmax*(44 + zmax)))) - 3*C6*(8 + zmax*(8 + zmax))*(256 + zmax*(512 + zmax*(320 + zmax*(64 + zmax)))))/zmax**6)/np.exp((2*z*(256*C6*(-60 + z*(30 + z*(-20 + z*(15 + 2*z*(-6 + 5*z))))) + 64*(C5 - 12*C6)*(60 + z*(-30 + z*(20 + 3*z*(-5 + 4*z))))*zmax + 80*(C4 - 10*C5 + 54*C6)*(-12 + z*(6 + z*(-4 + 3*z)))*zmax**2 + 40*(C3 - 8*C4 + 35*C5 - 112*C6)*(6 + z*(-3 + 2*z))*zmax**3 + 30*(C2 - 6*C3 + 20*C4 - 50*C5 + 105*C6)*(-2 + z)*zmax**4 + 15*(C1 - 4*C2 + 9*C3 - 16*C4 + 25*C5 - 36*C6)*zmax**5))/(5.*zmax**6))
 
 def get_samples_crossing_fde(z:np.ndarray,samples_gd:dict,order:int=4) -> dict:
-    """Get the dark energy evolution from MCMC samples of the Chebyshev coefficients
+    """Get the dark energy evolution $f_\mathrm{DE}(z)$ from MCMC samples of the Chebyshev coefficients
 
     Args:
         z (np.ndarray): array with redshift values
@@ -55,15 +55,11 @@ def get_samples_crossing_w(z,samples_gd:dict,order:int=4,return_fde:bool=True) -
     """
     Get the dark energy evolution.
 
-    .. math::
-
-        f_{\mathrm{DE}}(z) = \frac{\rho_{\mathrm{DE}}(z)}{\rho_{\mathrm{DE},0}}
+         $$f_\mathrm{DE}(z) = \frac{\rho_{\mathrm{DE}}(z)}{\rho_{\mathrm{DE},0}}$$
 
     for a given set of Chebyshev coefficients in the expansion of 
 
-    .. math::
-
-        w(z) = \sum_{i=0}^{N} c_i T_i(x)
+        $$w(z) = \sum_{i=0}^{N} c_i T_i(x)$$
 
     Args:
         z (_type_): _description_
