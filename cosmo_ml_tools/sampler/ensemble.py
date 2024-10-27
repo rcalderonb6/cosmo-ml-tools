efbc3efa6129b80ad69d9e582336ed2b5a65489d
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-27 12:11:27 +0100

    test

--- cosmo_ml_tools/sampler/ensemble.py
+++ cosmo_ml_tools/sampler/ensemble.py
@@ -6,8 +6,8 @@ class EnsembleBase(MCBase):
     Base Class for an Ensemble Sampler
     """
     def __init__(self,ini_file,engine:str,sampler_kwargs:dict):
-        super().__init__(ini_file,engine,sampler_kwargs)
-        self.info=self._initialize_helper(ini_file)
+        # super().__init__(ini_file,engine,sampler_kwargs)
+        self.info=_initialize_helper(ini_file)
         self.sampler_kwargs=sampler_kwargs
         self.out_dir=self.info['output'] if 'output' in self.info.keys() else None
         self._priors=None 
