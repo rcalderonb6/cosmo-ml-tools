6ac2268f0ec2503e26720514610be868fc93749c
rodri981 <rodrigocalderon10@hotmail.com>
2024-10-28 19:31:10 +0100

    add methods to analysis class

--- cosmo_ml_tools/analysis/analysis.py
+++ cosmo_ml_tools/analysis/analysis.py
@@ -12,6 +12,36 @@ class Analysis(AnalysisBase):
             self._chains.update(load_chains([chain],[label],root=root))
         else:
             pass
-        
+
+    def computeEvidence(self,chain:str=None) -> None:
+        pass
+    
+    
+    def load(self,chains:list[str]) -> None:
+        pass   
+    
+    
+    def add_chain(chain:str,label:str,root:str,index:int=-1):
+        pass    
+    
+    def plot_triangle(self,params:list[str]=None):
+        pass
+    
+    
+    def plot_2D(self,params:list[str]):
+        pass
+    
+    def plot_posterior_y(self,x,f,theta):
+        pass
+    
+    def _getGelmanRubin(self):
+        pass
+    
+    def getInfo(self):
+        pass
+    
+    def set_aliases(self,aliases:dict) -> None:
+        pass    
+            
 def load_chains(chains: list, labels: list, root: str='') -> dict:
     return {lbl: loadMCSamples(root+chain_fn) for lbl,chain_fn in zip(labels,chains)}
